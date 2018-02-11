8a54c694eb4b8b4a60e26180425122e15d92331e
Grekulov <grekulov.valentyn@gmail.com>
2018-02-11 19:32:21 +0200

    Written Readme, updated env logiv and mother file

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-Functional JS
+## Functional JS
+
+### Run app:
+* `npm start`
+* `go to http://localhost:3000 or indicate HOST and PORT in environment and follow http://HOST:PORT/ respectively`
+* `Set LESSON number (1, 2, 3...29) in environment. Otherwise all experiments will be runned at once`
+* `Check browser console to see lessons results`
--- build/functionalJS.js
+++ build/functionalJS.js
@@ -1,9 +1,19 @@
 import functionalJS from '../src';
 
-const lesson = 4;
+const lesson = LESSON;
 const requestStyle = 'background: blue; color: white; display: block;';
+const requestAllStyle = 'background: red; color: white; display: block;';
 const resultStyle = 'color: green; font-weight: bold;';
 
-console.log(`%c Run lesson ${lesson}`, requestStyle);
+const consoleRunLesson = n => console.log(`%c Run lesson ${n}`, requestStyle);
 
-functionalJS[`lesson${lesson}`](resultStyle);
+if (lesson) {
+  consoleRunLesson(lesson);
+  functionalJS[`lesson${lesson}`](resultStyle);
+} else {
+  console.log('%c Run all lessons', requestAllStyle);
+  Object.keys(functionalJS).map(key => {
+    consoleRunLesson(key);
+    functionalJS[key](resultStyle)
+  });
+}
